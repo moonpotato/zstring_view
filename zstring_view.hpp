880e34ff0dd9cbc26c4ab14cced2b51c70d0c071
moonpotato <7510150+moonpotato@users.noreply.github.com>
2018-06-09 19:00:53 +0800

    Add license notice to files

--- zstring_view.hpp
+++ zstring_view.hpp
@@ -1,3 +1,20 @@
+//  This file is part of moonpotato/zstring_view
+//  zstring_view: A non-owning reference to a null-terminated part of a string
+//
+//   Copyright 2018 moonpotato <git@funnelweb.xyz>
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
 #ifndef ZSTRING_VIEW_INCLUDED
 #define ZSTRING_VIEW_INCLUDED
 
